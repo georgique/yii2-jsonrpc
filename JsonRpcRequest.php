9675e2feee4a967bde1a702d2423857bceea70d2
George Shestayev <georgique@users.noreply.github.com>
2018-01-07 15:07:42 -0800

    Create JsonRpcRequest.php

---
+++ JsonRpcRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace georgique\yii2\jsonrpc;
+
+use yii\base\BaseObject;
+
+/**
+ * Class JsonRpcRequest
+ * Dumb class describing JSON-RPC request.
+ * @author George Shestayev george.shestayev@gmail.com
+ * @package georgique\yii2\jsonrpc
+ */
+class JsonRpcRequest extends BaseObject {
+
+    public $id;
+    public $route;
+    public $params;
+    public $originalRequest;
+
+}
